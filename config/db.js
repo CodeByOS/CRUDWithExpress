20668e6c9cec1432b9311ede87f9bafd6c3d062e
Oussama <saidi.oussama2004@gmail.com>
2025-04-25 05:54:45 +0100

    Update DB config

--- config/db.js
+++ config/db.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        const MONGO_URI = process.env.MONGO_URI;
-        await mongoose.connect(MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("DataBase is Connected..")
     } catch (error) {
         console.log("Failed to connect to Database..!", error);
